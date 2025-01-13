337d9ac60155b9f8f86e1605999e93bcafbc1d7c
Chiew Cheng Yi <150312205+ccy7701@users.noreply.github.com>
2025-01-13 22:47:53 +0800

    Commit dummy test

---
+++ tests/Functional/DummyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests;
+
+class ActivitiesFetchTest extends BaseTest
+{
+    /**
+     * @runInSeparateProcess
+     * Test fetching the user's activities by activity type
+     */
+    public function testEqualsToOne()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
